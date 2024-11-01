df391001430f640d5174d002ba91ecd2cdd84a6b
DanielSantosProg <danielsantos_lol@hotmail.com>
2024-11-01 09:06:56 -0300

    Adding Routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './modules/task-list/components/navbar/navbar.component';
-import { NewTaskComponent } from './modules/task-list/pages/new-task/new-task.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, NewTaskComponent],
-  template: `<app-navbar /> <app-new-task /> <router-outlet />`,
+  imports: [RouterOutlet, NavbarComponent],
+  template: `
+    <app-navbar></app-navbar>
+    <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent {
   title = 'Tasker';
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
 import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, Route } from '@angular/router';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { NewTaskComponent } from './app/modules/task-list/pages/new-task/new-task.component';
+import { ListComponent } from './app/modules/task-list/pages/list/list.component';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const routes: Route[] = [
+  { path: '', component: NewTaskComponent },
+  { path: 'list', component: ListComponent },
+];
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes), ...appConfig.providers],
+}).catch((err) => console.error(err));
