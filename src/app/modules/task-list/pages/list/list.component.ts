075dfd1d1abd68b05cb3ec4fed329a8d69b93bc6
DanielSantosProg <danielsantos_lol@hotmail.com>
2024-11-05 08:41:18 -0300

    Created the lists page

--- src/app/modules/task-list/pages/list/list.component.ts
+++ src/app/modules/task-list/pages/list/list.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss',
 })
-export class ListComponent {}
+export class ListComponent implements OnInit {
+  httpClient = inject(HttpClient);
+  data: any[] = [];
+
+  ngOnInit(): void {
+    this.fetchData();
+    this.data.forEach((list) => {
+      list.isDropdownVisible = true;
+    });
+  }
+
+  isDropdownVisible: boolean = true;
+  public toggleDropdown(list: any) {
+    list.isDropdownVisible = !list.isDropdownVisible;
+  }
+
+  fetchData() {
+    this.httpClient
+      .get('https://localhost:5001/api/TaskList')
+      .subscribe((data: any) => {
+        console.log(data);
+        this.data = data;
+      });
+  }
+}
